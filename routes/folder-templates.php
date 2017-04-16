6a0d562464b68444e4a82ffeb8ca093f76dd103e
Wellington Félix <wellington_bhmg@hotmail.com>
2017-04-16 00:22:58 -0300

    script php que define as rotas para a pasta templates/

--- routes/folder-templates.php
+++ routes/folder-templates.php
@@ -5,3 +5,9 @@
  * @author - wellington félix
  * @link   - wellington_bhmg@hotmail.com
  */
+
+# definindo constante com a rota do script php/html do template de cabeçalho
+define('HEADER_TEMPLATE', ABS_PATH . 'app/requires/templates/header.php');
+
+# definindo constante com a rota do script php/html do template de rodapé
+define('FOOTER_TEMPLATE', ABS_PATH . 'app/requires/templates/footer.php');
