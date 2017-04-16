126cd71787649a4f61e796aaa149cb16af50dc11
Wellington Félix <wellington_bhmg@hotmail.com>
2017-04-16 00:20:35 -0300

    templates de cabeçalho e rodapé

---
+++ app/requires/templates/footer.php
@@ -0,0 +1,16 @@
+  </main> <!-- /container -->
+
+  	<hr>
+  	<footer class="container">
+  		<p>&copy; 2017 - Contabilidade MSY LTDA</p>
+  	</footer>
+
+  	<script src="<?php echo BASE_URL; ?>libs/jquery/js/jquery.min.js"></script>
+
+    <script src="<?php echo BASEURL; ?>libs/bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="<?php echo BASEURL; ?>js/main.js"></script>
+
+  </body>
+
+</html>
