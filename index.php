2ef1748d0d43ab7cb9c18b97261143dddf56c243
Wellington Félix <wellington_bhmg@hotmail.com>
2017-04-16 00:24:04 -0300

    criando dashboard

--- index.php
+++ index.php
@@ -1,17 +1,53 @@
 <?php require 'init-all-routes.php' ?>
-<?php require CONNECTION_SETTINGS_PATH ?>
-<?php require CONNECTION_PATH ?>
 
-<?php
-  $db = open_database();
+<?php require CONNECTION_SETTINGS_PATH; ?>
+<?php require CONNECTION_PATH; ?>
 
-  if ($db) {
+<?php include (HEADER_TEMPLATE); ?>
 
-    echo '<h1>Conectado ao Banco de Dados!</h1>';
+<?php $db = open_database(); ?>
 
-  } else {
+<h1>Dashboard</h1>
+<hr>
 
-    echo '<h1>ERRO: Não foi possível conectar!</h1>';
+<?php if ($db) : ?>
 
-  }
-?>
+  <div class="row">
+
+    <div class="col-xs-6 col-sm-3 col-md-2">
+      <a href="#" class="btn btn-primary">
+        <div class="row">
+          <div class="col-xs-12 text-center">
+            <i class="fa fa-plus fa-5x"></i>
+          </div>
+          <div class="col-xs-12 text-center">
+            <p>Novo Contato</p>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="col-xs-6 col-sm-3 col-md-2">
+      <a href="#" class="btn btn-default">
+        <div class="row">
+          <div class="col-xs-12 text-center">
+            <i class="fa fa-user fa-5x"></i>
+          </div>
+          <div class="col-xs-12 text-center">
+            <p>Contatos</p>
+          </div>
+        </div>
+      </a>
+    </div>
+
+  </div>
+
+<?php else : ?>
+
+  <div class="alert alert-danger" role="alert">
+    <p><strong>ERRO: </strong>Não foi possível conectar ao banco de dados!</p>
+  </div>
+
+<?php endif; ?>
+
+<?php include (FOOTER_TEMPLATE); ?>
