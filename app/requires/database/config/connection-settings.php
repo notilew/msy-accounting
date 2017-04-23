c54b71f315b7ee215b9e47d8d537d5ab873135b2
Wellington Félix <wellington_bhmg@hotmail.com>
2017-04-23 14:41:56 -0300

    script de configuração para conexão com o banco de dados

---
+++ app/requires/database/config/connection-settings.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * script que define as constantes com as configurações para conexão com o SGBD MYSQL e a base de dados da contabilidade MSY
+ * @author - wellington félix
+ * @link   - wellington_bhmg@hotmail.com
+ */
+
+# definindo constante com o nome da base de dados da contabilidade MSY
+define('DB_NAME', 'msy_accounting');
+
+# definindo constante com o nome do usuário do SGBD MYSQL
+define('DB_USER', 'root');
+
+# definindo constante com a senha do usuário do SGBD MYSQL
+define('DB_PASSWORD', 'super');
+
+# definindo constante com o nome do host
+define('DB_HOST', 'localhost');
